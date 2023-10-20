e5ef63baf3615f1141d88f33884aa988bedc26f2
jwapico <altizer16@outlook.com>
2023-10-19 23:32:03 -0600

    Refactor Nannou Model

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use nannou::prelude::*;
 
 // Things that can be drawn to the screen
 trait Nannou {
-    fn display(&self, app: &App, model: &Model);
+    fn display(&self, app: &App, _model: &Model);
     fn update(&mut self);
 }
 
@@ -12,16 +12,37 @@ struct Pos {
     y: f32,
 }
 
+impl Default for Pos {
+    fn default() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+}
+
 // The dimensions of a rectangle
 struct Dimensions {
     w: i32,
     h: i32,
 }
 
+impl Default for Dimensions {
+    fn default() -> Self {
+        Self { w: 25, h: 25 }
+    }
+}
+
 // A direction of movement, represented in polar coordinates
 struct Direction {
     speed: f32,
-    angle: f32,
+    degrees: f32,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Self {
+            speed: 0.0,
+            degrees: 0.0,
+        }
+    }
 }
 
 // A single ant
@@ -29,68 +50,81 @@ struct Ant {
     pos: Pos,
     size: Dimensions,
     dir: Direction,
+    texture: wgpu::Texture,
+}
+
+impl Ant {
+    fn new(texture: wgpu::Texture) -> Self {
+        Self {
+            pos: Pos::default(),
+            size: Dimensions::default(),
+            dir: Direction::default(),
+            texture,
+        }
+    }
 }
 
 impl Nannou for Ant {
-    fn display(&self, app: &App, model: &Model) {
+    fn display(&self, app: &App, _model: &Model) {
         let draw = app.draw();
 
-        draw.texture(&model.texture)
+        draw.texture(&self.texture)
             .x_y(self.pos.x, self.pos.y)
             .w_h(self.size.w as f32, self.size.h as f32);
     }
 
-    fn update(&mut self) {}
+    fn update(&mut self) {
+        self.pos.x += deg_to_rad(self.dir.degrees).cos() * self.dir.speed;
+        self.pos.y += deg_to_rad(self.dir.degrees).sin() * self.dir.speed;
+    }
 }
 
 // The model of the nannou app
 struct Model {
-    texture: wgpu::Texture,
-    last_update_time: std::time::Instant,
-    update_interval: std::time::Duration,
+    _window: window::Id,
     ant: Ant,
 }
 
-impl Nannou for Model {
-    fn display(&self, app: &App, model: &Model) {}
+impl Model {
+    fn new(_window: window::Id, ant_texture: wgpu::Texture) -> Self {
+        Self {
+            _window,
+            ant: Ant::new(ant_texture),
+        }
+    }
+}
 
-    fn update(&mut self) {}
+impl Nannou for Model {
+    fn display(&self, app: &App, _model: &Model) {
+        self.ant.display(app, _model)
+    }
+
+    fn update(&mut self) {
+        self.ant.update();
+    }
 }
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
+//
+// Nannou Interface
+//
+
 // Initialize the Model and perform setup tasks. This is run once on startup
 fn model(app: &App) -> Model {
-    app.new_window().size(512, 512).view(view).build().unwrap();
+    let _window = app.new_window().size(512, 512).view(view).build().unwrap();
+
     let assets = app.assets_path().unwrap();
     let img_path = assets.join("red_ant.png");
     let ant_texture = wgpu::Texture::from_path(app, img_path).unwrap();
 
-    let ant = Ant {
-        pos: Pos { x: 0.0, y: 0.0 },
-        size: Dimensions { w: 30, h: 30 },
-        dir: Direction {
-            speed: 10.0,
-            angle: 0.0,
-        },
-    };
-
-    Model {
-        texture: ant_texture,
-        ant,
-        update_interval: std::time::Duration::from_millis(100), // Adjust the interval as needed
-        last_update_time: std::time::Instant::now(),
-    }
+    Model::new(_window, ant_texture)
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    let current_time = std::time::Instant::now();
-
-    if current_time - model.last_update_time >= model.update_interval {
-        model.last_update_time = current_time;
-    }
+    model.update()
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -99,9 +133,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     frame.clear(background_color);
 
-    draw.texture(&model.texture)
-        .x_y(model.ant.pos.x, model.ant.pos.y)
-        .w_h(model.ant.size.w as f32, model.ant.size.h as f32);
+    model.display(app, model);
 
     draw.to_frame(app, &frame).unwrap();
 }

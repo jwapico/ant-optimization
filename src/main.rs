e5758d17846e55af2d8daba77c349eb9efe8befa
jwapico <altizer16@outlook.com>
2023-10-20 01:18:22 -0600

    More refactors and comments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use nannou::prelude::*;
 
-// Things that can be drawn to the screen
+const WINDOW_SIZE: u32 = 1080;
+
+// for types that need to be drawn to the screen
 trait Nannou {
     fn display(&self, app: &App, _model: &Model);
     fn update(&mut self);
 }
 
-// A position on the screen
+// position on the screen
+#[derive(Default)]
 struct Pos {
     x: f32,
     y: f32,
 }
 
-impl Default for Pos {
-    fn default() -> Self {
-        Self { x: 0.0, y: 0.0 }
-    }
+// direction in polar coordinates
+#[derive(Default)]
+struct Direction {
+    speed: f32,
+    degrees: f32,
 }
 
-// The dimensions of a rectangle
+// dimensions of a rectangle, defaults to 25px
 struct Dimensions {
     w: i32,
     h: i32,
@@ -26,26 +30,11 @@ struct Dimensions {
 
 impl Default for Dimensions {
     fn default() -> Self {
-        Self { w: 25, h: 25 }
+        Self { w: 15, h: 15 }
     }
 }
 
-// A direction of movement, represented in polar coordinates
-struct Direction {
-    speed: f32,
-    degrees: f32,
-}
-
-impl Default for Direction {
-    fn default() -> Self {
-        Self {
-            speed: 0.0,
-            degrees: 0.0,
-        }
-    }
-}
-
-// A single ant
+// an ant is made up of a position, direction, size, and texture (ant png).
 struct Ant {
     pos: Pos,
     size: Dimensions,
@@ -65,6 +54,7 @@ impl Ant {
 }
 
 impl Nannou for Ant {
+    // draws the Ant to the screen
     fn display(&self, app: &App, _model: &Model) {
         let draw = app.draw();
 
@@ -73,13 +63,17 @@ impl Nannou for Ant {
             .w_h(self.size.w as f32, self.size.h as f32);
     }
 
+    // updates the Ant
     fn update(&mut self) {
+        // change the x position by the x component of direction
         self.pos.x += deg_to_rad(self.dir.degrees).cos() * self.dir.speed;
+
+        // change the y position by the y component of direction
         self.pos.y += deg_to_rad(self.dir.degrees).sin() * self.dir.speed;
     }
 }
 
-// The model of the nannou app
+// describes the state of the app
 struct Model {
     _window: window::Id,
     ant: Ant,
@@ -104,29 +98,39 @@ impl Nannou for Model {
     }
 }
 
-fn main() {
-    nannou::app(model).update(update).run();
-}
-
 //
 // Nannou Interface
 //
 
-// Initialize the Model and perform setup tasks. This is run once on startup
-fn model(app: &App) -> Model {
-    let _window = app.new_window().size(512, 512).view(view).build().unwrap();
+fn main() {
+    nannou::app(model).update(update).run();
+}
 
+// make a new model. this is run once when the nannou app is started
+fn model(app: &App) -> Model {
+    // create the window
+    let _window = app
+        .new_window()
+        .size(WINDOW_SIZE, WINDOW_SIZE)
+        .view(view)
+        .build()
+        .unwrap();
+
+    // load the ant texture
     let assets = app.assets_path().unwrap();
     let img_path = assets.join("red_ant.png");
     let ant_texture = wgpu::Texture::from_path(app, img_path).unwrap();
 
+    // return a new Model with the window and ant texture
     Model::new(_window, ant_texture)
 }
 
+// this is run on timed updates, 60 times per second
 fn update(_app: &App, model: &mut Model, _update: Update) {
     model.update()
 }
 
+// draw to the screen
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let background_color = rgb(34.0 / 255.0, 40.0 / 255.0, 49.0 / 255.0);
